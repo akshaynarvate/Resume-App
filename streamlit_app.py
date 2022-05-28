0b180b555fb8297b49c72d39b6b024df571b92b6
Akshay Narvate <88389553+akshaynarvate@users.noreply.github.com>
2022-05-28 20:13:36 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,11 +13,11 @@ st.write('''
 ##### *Resume* 
 ''')
 
-#image = Image.open('dp.png')
-#st.image(image, width=150)
+image = Image.open('dp.png')
+st.image(image, width=150)
 
 st.markdown('## Summary', unsafe_allow_html=True)
-#st.info('''
+st.info('''
 - Experienced Educator, Researcher and Administrator with almost twenty years of experience in data-oriented environment and a passion for delivering insights based on predictive modeling. 
 - Strong verbal and written communication skills as demonstrated by extensive participation as invited speaker at `10` conferences as well as publishing 149 research articles.
 - Strong track record in scholarly research with H-index of `32` and total citation of 3200+.
