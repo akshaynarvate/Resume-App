e4b04fff5cd0894ed6eb865f1194c2e90e634df7
Akshay Narvate <88389553+akshaynarvate@users.noreply.github.com>
2022-06-24 13:41:47 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,7 +9,7 @@ with open("style.css") as f:
 #####################
 # Header 
 st.write('''
-# Chanin Nantasenamat, Ph.D.
+# Akshay Narvate, Data Scientist.
 ##### *Resume* 
 ''')
 
@@ -18,9 +18,15 @@ st.image(image, width=150)
 
 st.markdown('## Summary', unsafe_allow_html=True)
 st.info('''
-- Experienced Educator, Researcher and Administrator with almost twenty years of experience in data-oriented environment and a passion for delivering insights based on predictive modeling. 
-- Strong verbal and written communication skills as demonstrated by extensive participation as invited speaker at `10` conferences as well as publishing 149 research articles.
-- Strong track record in scholarly research with H-index of `32` and total citation of 3200+.
+- Experienced Data Scientist with almost 0.6 years of experience in data-oriented environment and a passion for delivering insights based on predictive modeling. 
+- Strong verbal and written communication skills as worked closely with business partners.
+- Developed customised models for various business problems and designed the prototypes from problem identification to model deployment to increase the
+  financial gains and the efficiency.
+- Experience in developing statistical/Machine Learning models
+  using various techniques and technologies using R, Python.
+- Provided EDA to uncover meaningful patterns out of data using R/Python.
+- Ensuring that analysis are delivered on time with more focussed on client &
+  business satisfaction.
 ''')
 
 #####################
@@ -46,7 +52,7 @@ st.markdown("""
         <a class="nav-link" href="#work-experience">Work Experience</a>
       </li>
       <li class="nav-item">
-        <a class="nav-link" href="#bioinformatics-tools">Bioinformatics Tools</a>
+        <a class="nav-link" href="#bioinformatics-tools">Projects</a>
       </li>
       <li class="nav-item">
         <a class="nav-link" href="#social-media">Social Media</a>
@@ -93,126 +99,84 @@ st.markdown('''
 ## Education
 ''')
 
-txt('**Doctor of Philosophy** (Medical Technology), *Mahidol University*, Thailand',
-'2002-2006')
+txt('**Bachelors of Science** (Biological Science), *Bhavans Vivekananda College*, Hyderabad',
+'2015-2018')
 st.markdown('''
-- GPA: `3.89`
-- Research thesis entitled `Computer-aided molecular design for biological and chemical applications : Quantum chemical and machine learning approach`.
-- Received Royal Golden Jubilee Ph.D. Scholarship of `2.152 million THB` covering tuition and stipend.
-- Thesis awarded `1st` Prize by the National Research Council of Thailand.
-''')
+- Percentage: `66`
+- Graduated with First Class.''')
 
-txt('**Bachelors of Science** (Biological Science), *Mahidol University International College*, Thailand',
-'1998-2002')
-st.markdown('''
-- GPA: `3.65`
-- Graduated with First Class Honors.
-''')
 
 #####################
 st.markdown('''
 ## Work Experience
 ''')
 
-txt('**Head, Center of Data Mining and Biomedical Informatics**, Faculty of Medical Technology, Mahidol University, Thailand',
-'2011-2021')
+txt('**Data Science Intern**, Bepec Solutions, Bangalore, India',
+'2022-Present')
+txt('**Data Science Real life Projects:-**)
+txt('**Next Best Action**, Churn Customer, Standard Bank, Mozambique',
+'2022')
 st.markdown('''
-- Managing a Center of `10` professors, researchers and students to ensure KPIs are strategically achieved namely to publish at least `20+` research publications annually. 
-- Actively took part in the talent hiring process as well as help employees to plan and develop their career path.
-- Set budget and handle procurement in order to facilitate education and research activities. Secured `> 10 million THB` budget.
-- Set and reflect on OKR on an annual basis to ensure productivity strategically matches the organization's direction.
+- Increased the *Revenue* of Standard Bank by *1.2%* by *decreasing* the *Customer churn* rate by *5%*.
+-*Proactively finding* out about the *Churn Customer*.
+- Done *EDA* to uncover *meaningful patterns* out of data using Python.
+- Developed *customised model* for *business problem* to increase the
+ *financial gains* & *efficiency*.
+-*Timely* delivery of *Analysis* more focussed on *client* &
+ *business satisfaction*.
+- Transformed business by *creating value* out of data using *analytics*.''')
+
+txt('**Recommendation Engine**, Bank Product Recommendation Engine, Standard Bank, Mozambique',
+'2022')
+st.markdown('''
+- Developed *Collaborative Filtering Recommendation Engine* which is User centric by understanding *patterns in ratings*.
+- Created Proof of Concept for business demonstration.
+- Based on *pattern understanding* in ratings *similar products* are recommended which interests them.
 ''')
 
-txt('**Associate Professor**, Faculty of Medical Technology, Mahidol University, Thailand',
-'2012-2021')
-txt('**Assistant Professor**, Faculty of Medical Technology, Mahidol University, Thailand',
-'2009-2012')
-txt('**Lecturer**, Faculty of Medical Technology, Mahidol University, Thailand',
-'2006-2009')
+txt('**Fitness Prediction App**, Gym App, India',
+'2022')
 st.markdown('''
-- Provided mentorship and supervision to junior faculty, researchers, Ph.D./M.Sc./B.Sc. students. Mentored `3` Post-doctoral fellows, supervised `13` Ph.D. students, supervised `8` M.Sc. students, supervised `13` B.Sc. students and hosted `6` visiting students from U.S., Sweden and India.
-- Wrote and applied for research grants. Served as Principal Investigator for research grants that have been awarded `12.5 million THB` and `1.117 million SEK` in research funding from Thai and Swedish grant agencies.
-- Conducted research by applying machine learning to computational drug discovery and ensuring that research output exceeds `20+` articles per year.
-- Taught `10+` undergraduate/graduate classes on Bioinformatics, Data Mining, Scientific Research and Presentation, Research Methodology, Graduate Seminar, Programming for Health Data Science, etc.
-- Peer reviewed `100+` research articles for leading scientific journals.
+- Developed customised Proof of Concept that helps predict health status based on inputs.
+- Extracted meaningful hidden patterns and flawlessly predict outcome.
+- Fully Deployed fitness prediction app to predict outcome based on age, height, weight.
 ''')
 
-txt('**Co-Chair**, International Conference on Pharmaceutical Bioinformatics at Pattaya, Thailand',
-'2016')
+txt('**Technical Writer**, [Akshay Narvate Blog](https://medium.com/@akshaynarvate786123) on Medium.com',
+'2022-Present')
 st.markdown('''
-- Oversee all aspects of the conference preparations from conception to launch. This include inviting keynote and plenary speakers, create advertisement flyers, create abstract book, etc.
-- Conference attracted `200+` participants from `40+` countries from university and industry sector.
-- Chaired keynote session, technical workshop and some of the parallel sessions.
-''')
-
-txt('**Content Creator**, [Data Professor YouTube Channel](https://youtube.com/dataprofessor/)',
-'2019-Present')
-st.markdown('''
-- `100,000+` subscribers on YouTube
-- Created `261` educational videos on data science, machine learning and bioinformatics as well as hosted several podcast episodes with data scientists.
-- Created `3` sponsored videos for Notion, Gradio and Classpert.
-''')
-
-txt('**Content Creator**, [Coding Professor YouTube Channel](https://youtube.com/codingprofessor/)',
-'2019-Present')
-st.markdown('''
-- `3,200+` subscribers on YouTube
-- Created `38` educational videos on Python and R programming.
-''')
-
-txt('**Technical Writer**, [Data Professor Blog](https://data-professor.medium.com/) on Medium.com',
-'2019-Present')
-st.markdown('''
-- `4,100+` subscribers on Medium
-- Written `68` technical blogs on data science, machine learning and bioinformatics.
+- `2+` subscribers on Medium
+- Written `3` blogs on data science, machine learning, business and mindset.
 ''')
 
 #####################
 st.markdown('''
-## Bioinformatics Tools
+## Data Science Projects
 ''')
-txt4('ABCpred', 'A web server for the discovery of acetyl- and butyryl-cholinesterase inhibitors', 'http://codes.bio/abcpred/')
-txt4('AutoWeka', 'An automated data mining software based on Weka', 'http://www.mt.mahidol.ac.th/autoweka/')
-txt4('ACPred', 'A computational tool for the prediction and analysis of anticancer peptides','http://codes.bio/acpred/')
-txt4('BioCurator', 'A web server for curating ChEMBL bioactivity data', 'http://codes.bio/biocurator/')
-txt4('CryoProtect', 'A web server for classifying antifreeze proteins from non-antifreeze proteins','http://codes.bio/cryoprotect/')
-txt4('ERpred', 'A web server for the prediction of subtype-specific estrogen receptor antagonists', 'http://codes.bio/erpred')
-txt4('HCVpred', 'A web server for predicting the bioactivity of Hepatitis C virus NS5B inhibitors', 'http://codes.bio/hemopred/')
-txt4('HemoPred', 'A web server for predicting the hemolytic activity of peptides', 'http://codes.bio/hemopred/')
-txt4('iQSP', 'A sequence-based tool for the prediction and analysis of quorum sensing peptides', 'http://codes.bio/iqsp/')
-txt4('Meta-iAVP', 'A sequence-based meta-predictor for improving the prediction of antiviral peptides', 'http://codes.bio/meta-iavp/')
-txt4('osFP', 'A web server for predicting the oligomeric state of fluorescent proteins', 'http://codes.bio/osfp/')
-txt4('PAAP', 'A web server for predicting antihypertensive activity of peptides', 'http://codes.bio/paap/')
-txt4('PepBio', 'A web server for predicting the bioactivity of host defense peptide', 'http://codes.bio/pepbio')
-txt4('PyBact', 'Open source software written in Python for bacterial identification', 'https://sourceforge.net/projects/pybact/')
-txt4('TargetAntiAngio', 'A sequence-based tool for the prediction and analysis of anti-angiogenic peptides','http://codes.bio/targetantiangio/')
-txt4('ThalPred', 'Development of decision model for discriminating Thalassemia trait and Iron deficiency anemia','http://codes.bio/thalpred/')
-txt4('THPep', 'A web server for predicting tumor homing peptides','http://codes.bio/thpep/')
-
+txt4('Next Best Action Standard Bank', 'This is an supervised Machine Learning project that uses Random forest method that proactively finds out the churn customer.', 'https://github.com/akshaynarvate/Next-Best-Action')    
+txt4('Bank Product Recommendation Engine', 'This is an unsupervised Machine Learning project that uses Collaborative Filtering method that recommends products based on the similar customer interests.', 'https://share.streamlit.io/akshaynarvate/product-recommendation-engine/main/main.py', 'https://github.com/akshaynarvate/Bank-Product-Recommendation-engine')
+txt4('Fitness Prediction Application', 'Gym-App is a fully deployed Machine Learning model that helps you predict your health status based on your inputs.', 'https://share.streamlit.io/akshaynarvate/gymapp/main/main.py'. 'https://github.com/akshaynarvate/Gymapp')
+txt4('Stock Price Prediction with LSTM', 'Reliance Stock Prediction is Deep Learning LSTM model that predicts stock closing price using LSTM based on the Open, High, Low and Volume as inputs.','https://share.streamlit.io/akshaynarvate/stock-price-prediction-lstm/main/main.py', 'https://github.com/akshaynarvate/Stock-Price-Prediction-LSTM')
+txt4('Health Insurance Premium Prediction App', 'Health Insurance Premium Amount Prediction uses Machine learning predicts premium amount using Linear Regression taking age, sex, bmi, smoker as inputs.', 'https://share.streamlit.io/akshaynarvate/health-insurance-premium-prediction/main/main.py', 'https://github.com/akshaynarvate/Health-Insurance-Premium-Prediction')
+txt4('Future Sales Prediction', 'Application predicts future sales using Machine Learning taking TV and Radio advertising budget as input.','https://share.streamlit.io/akshaynarvate/future-sales-prediction/main/main.py', 'https://github.com/akshaynarvate/Future-Sales-Prediction')
+    
 
 #####################
 st.markdown('''
 ## Skills
 ''')
-txt3('Programming', '`Python`, `R`, `Linux`')
+txt3('`Python`, `R`')
 txt3('Data processing/wrangling', '`SQL`, `pandas`, `numpy`')
-txt3('Data visualization', '`matplotlib`, `seaborn`, `plotly`, `altair`, `ggplot2`')
+txt3('Data visualization', '`matplotlib`, `seaborn`, `plotly`, `ggplot2`')
 txt3('Machine Learning', '`scikit-learn`')
-txt3('Deep Learning', '`TensorFlow`')
-txt3('Web development', '`Flask`, `HTML`, `CSS`')
-txt3('Model deployment', '`streamlit`, `gradio`, `R Shiny`, `Heroku`, `AWS`, `Digital Ocean`')
+#txt3('Deep Learning', '`TensorFlow`')
+txt3('Model deployment', '`streamlit`')
 
 #####################
 st.markdown('''
 ## Social Media
 ''')
-txt2('LinkedIn', 'https://www.linkedin.com/in/chanin-nantasenamat')
-txt2('Twitter', 'https://twitter.com/thedataprof')
-txt2('GitHub', 'https://github.com/chaninn/')
-txt2('', 'https://github.com/chaninlab/')
-txt2('', 'https://github.com/dataprofessor')
-txt2('ORCID', 'http://orcid.org/0000-0003-1040-663X')
-txt2('Scopus', 'http://www.scopus.com/authid/detail.url?authorId=12039071300')
-txt2('ResearcherID', 'http://www.researcherid.com/rid/F-1021-2010')
-txt2('ResearchGate', 'https://www.researchgate.net/profile/Chanin_Nantasenamat')
-txt2('Publons', 'https://publons.com/a/303133/')
+txt2('LinkedIn', 'https://www.linkedin.com/in/akshaynarvate')
+txt2('GitHub', 'https://github.com/akshaynarvate')
+txt2('Email', 'akshaynarvate@proton.me')
+txt2('Medium', 'https://medium.com/@akshaynarvate786123')
